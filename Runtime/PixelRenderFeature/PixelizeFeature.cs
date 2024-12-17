8c1a7d216920f8377433397c75d7f99d40231872
Draginalo <tropicaltisa@gmail.com>
2024-12-17 11:59:41 -0500

    Reverted.

---
+++ Runtime/PixelRenderFeature/PixelizeFeature.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace PSX.ToolKit {
+    public class PixelizeFeature : ScriptableRendererFeature
+    {
+        [System.Serializable]
+        public class CustomPassSettings
+        {
+            public RenderPassEvent mRenderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+            public int mScreenHeight = 144;
+        }
+
+        [SerializeField] private CustomPassSettings mPassSettings;
+        private PixelizePass mPixelizePass;
+
+        /// <inheritdoc/>
+        public override void Create()
+        {
+            mPixelizePass = new PixelizePass(mPassSettings);
+        }
+
+        // Here you can inject one or multiple render passes in the renderer.
+        // This method is called when setting up the renderer once per-camera.
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(mPixelizePass);
+        }
+    }
+}
+
+
