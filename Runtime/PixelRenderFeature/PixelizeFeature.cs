dc320638e4734dcb3899a963344a6309c640efa1
Draginalo <tropicaltisa@gmail.com>
2024-12-17 10:55:39 -0500

    Fixed assembly defenition reference for pixelize render feature.

--- Runtime/PixelRenderFeature/PixelizeFeature.cs
+++ Runtime/PixelRenderFeature/PixelizeFeature.cs
@@ -2,29 +2,31 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class PixelizeFeature : ScriptableRendererFeature
-{
-    [System.Serializable]
-    public class CustomPassSettings
+namespace PSX.ToolKit {
+    public class PixelizeFeature : ScriptableRendererFeature
     {
-        public RenderPassEvent mRenderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
-        public int mScreenHeight = 144;
-    }
+        [System.Serializable]
+        public class CustomPassSettings
+        {
+            public RenderPassEvent mRenderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+            public int mScreenHeight = 144;
+        }
 
-    [SerializeField] private CustomPassSettings mPassSettings;
-    private PixelizePass mPixelizePass;
+        [SerializeField] private CustomPassSettings mPassSettings;
+        private PixelizePass mPixelizePass;
 
-    /// <inheritdoc/>
-    public override void Create()
-    {
-        mPixelizePass = new PixelizePass(mPassSettings);
-    }
+        /// <inheritdoc/>
+        public override void Create()
+        {
+            mPixelizePass = new PixelizePass(mPassSettings);
+        }
 
-    // Here you can inject one or multiple render passes in the renderer.
-    // This method is called when setting up the renderer once per-camera.
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-    {
-        renderer.EnqueuePass(mPixelizePass);
+        // Here you can inject one or multiple render passes in the renderer.
+        // This method is called when setting up the renderer once per-camera.
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(mPixelizePass);
+        }
     }
 }
 
